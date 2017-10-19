1573935fc3b61636cf1c6a719d126e9d58571afa
sgy <sgy@sgy.local>
2017-10-19 13:29:09 +0800

    add build package

--- config/webpackServer.js
+++ config/webpackServer.js
@@ -1,4 +1,4 @@
-var config = require("../webpack.config.js");
+var config = require("./webpack.config.js");
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var path = require('path');
